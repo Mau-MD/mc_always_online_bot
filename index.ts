1d639b37f182fd706d4d4a3edc6c631a05e272ab
Mau MD <j.mauricio.munoz1@gmail.com>
2022-06-06 13:01:33 -0700

    better mvoement

--- index.ts
+++ index.ts
@@ -9,56 +9,92 @@ const PORT = process.env.PORT || 3000;
 
 // BOT
 
+let botNum = 1;
+
 const host = process.env.MINECRAFT_HOST || "";
 const username = process.env.MINECRAFT_USERNAME || "";
 const port = process.env.MINECRAFT_PORT;
 
+// VARS
+const actions: Array<"forward" | "back" | "left" | "right"> = [
+  "forward",
+  "back",
+  "left",
+  "right",
+];
+
+let lastTime = -1;
+let isMoving = false;
+let isConnected = false;
+let lastAction: typeof actions[number];
+
+const moveInterval = 2;
+const maxRandom = 5;
+
 const botConfig = {
   host,
-  username,
+  username: username + botNum,
   port: port ? parseInt(port) : 25565,
 };
 
-const bot = mineflayer.createBot(botConfig);
+let bot = mineflayer.createBot(botConfig);
 
-bot.once("spawn", () => {
-  setInterval(moveRandomly, 2000);
+bindEvents(bot);
 
-  let movementIndex = 0;
+const handleMovement = (bot: mineflayer.Bot) => {
+  if (!isConnected) return;
 
-  function moveRandomly() {
-    if (movementIndex % 2 === 0) bot.clearControlStates();
-
-    switch (Math.floor(movementIndex / 2)) {
-      case 0:
-        bot.setControlState("forward", true);
-        break;
-      case 1:
-        bot.setControlState("back", true);
-        break;
-      case 2:
-        bot.setControlState("left", true);
-        break;
-      case 3:
-        bot.setControlState("right", true);
-        break;
+  var randomaAdd = Math.random() * maxRandom * 20;
+  var interval = moveInterval * 20 + randomaAdd;
+  if (bot.time.age - lastTime > interval) {
+    if (isMoving) {
+      bot.setControlState(lastAction, false);
+      isMoving = false;
+      lastTime = bot.time.age;
+    } else {
+      var yaw = Math.random() * Math.PI - 0.5 * Math.PI;
+      var pitch = Math.random() * Math.PI - 0.5 * Math.PI;
+      bot.look(yaw, pitch, false);
+      lastAction = actions[Math.floor(Math.random() * actions.length)];
+      bot.setControlState(lastAction, true);
+      isMoving = true;
+      lastTime = bot.time.age;
+      bot.activateItem();
     }
-    movementIndex += 1;
-    movementIndex = movementIndex % 8;
   }
-});
+};
 
-bot.on("kicked", () => {
-  bot.connect(botConfig);
-});
+function bindEvents(bot: mineflayer.Bot) {
+  bot.once("spawn", () => {
+    isConnected = true;
+    lastTime = bot.time.age;
+  });
 
-bot.on("error", () => {
-  bot.connect(botConfig);
-});
+  bot.on("time", () => {
+    handleMovement(bot);
+  });
 
-bot.once("spawn", () => {
-  console.log("Bot spawned!");
-});
+  bot.on("error", (err) => {
+    console.log("Error: " + err.message);
+    isConnected = false;
+    botNum += 1;
+    setTimeout(relog, 5000);
+  });
+
+  bot.on("end", (a) => {
+    isConnected = false;
+    console.log("Bot has ended: " + a);
+    botNum += 1;
+    setTimeout(relog, 5000);
+  });
+}
+
+function relog() {
+  console.log("Attempting to reconnect...");
+  bot = mineflayer.createBot(botConfig);
+  bindEvents(bot);
+}
+
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
